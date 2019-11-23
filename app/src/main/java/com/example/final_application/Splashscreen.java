169843409607fca340eafae5cd5afbe8bc8adf58
AndyOmondi17 <andyomondi17@gmail.com>
2019-11-23 16:17:41 +0300

    create logic behind the splashScreen

--- app/src/main/java/com/example/final_application/Splashscreen.java
+++ app/src/main/java/com/example/final_application/Splashscreen.java
@@ -1,4 +1,28 @@
 package com.example.final_application;
 
-public class Splashscreen {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),Login.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
 }
