b6b5f842cfa29bc7a3c5467cec4ff0f9db6f8959
AndyOmondi17 <andyomondi17@gmail.com>
2019-11-23 18:38:43 +0300

    create the navigation view on item listener

--- app/src/main/java/com/example/final_application/MainActivity.java
+++ app/src/main/java/com/example/final_application/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.final_application;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
+    @Override
+    public boolean onNavigationItemSelected(@Nullable MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.nav_schedule:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CalendarView()).commit();
+
+                break;
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new createEvent()).commit();
+                break;
+
+            case R.id.nav_announcement:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ViewEvent()).commit();
+                break;
+//            case R.id.message:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new NotificationsFragment()).commit();
+//                break;
+//            case R.id.nav_settings:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new NotificationsFragment()).commit();
+//                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
